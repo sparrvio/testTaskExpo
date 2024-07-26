540e864cc4cbd4809d16dbec7ea34e75eecb8450
sparrvio <sparrvio@.ya.ru>
2024-07-26 17:19:04 +0300

    finish

---
+++ src/main/java/testTaskExpa/EqualsWeight.java
@@ -0,0 +1,35 @@
+package testTaskExpa;
+
+import java.util.List;
+
+public class EqualsWeight {
+    private List<Integer> list;
+    private int totalWeight;
+
+    public EqualsWeight(List<Integer> list, int totalWeight) {
+        this.list = list;
+        this.totalWeight = totalWeight;
+    }
+
+    public void maxCarrots() {
+        int result = 0;
+        int length = list.size();
+        for (int i = 0; i < 10; ++i) {
+            int weight = 0;
+            for (int j = length; j > 0; j--) {
+                if (weight + j > 5 || list.get(j - 1) == 0) {
+                    continue;
+                } else {
+                    weight += j;
+                    list.set(j - 1, list.get(j - 1) - j);
+                    j++;
+                }
+            }
+            if (weight > 0) {
+                result += weight;
+            }
+        }
+        System.out.println("\nОбщий вес моркови на всех полянках: " + totalWeight +  " кг,\n" +
+                "из них за 10 ходок заяц сможет унести " + result + " кг.");
+    }
+}
---
+++ src/main/java/testTaskExpa/TestKeys.java
@@ -0,0 +1,35 @@
+package testTaskExpa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+        Для решения этой задачи подойдет т.н. жадный алгоритм.
+        Каждый раз мы будем принимать локально лучшее решение с расчетом на то,
+        что в итоге это даст нам лучшее решение на глобальном уровне.
+*/
+
+public class TestKeys {
+    public static void main(String[] args) {
+        System.out.println("Введите количество морковок на полянках в штуках начиная с полянки № 1");
+        List<Integer> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int totalWeight = 0;
+        for (int i = 0; i < 5; i++) {
+            if (scanner.hasNextInt()) {
+                list.add(scanner.nextInt() * (i + 1));
+                totalWeight += list.get(i);
+                System.out.println("На полянке № " + (i + 1) + ": " + list.get(i) + " килограмм моркови");
+            } else {
+                i--;
+                System.out.println("Некорректный ввод");
+                scanner.next();
+                continue;
+            }
+        }
+
+        EqualsWeight weight = new EqualsWeight(list, totalWeight);
+        weight.maxCarrots();
+    }
+}
